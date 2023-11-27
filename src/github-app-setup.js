0f1739ab953612754d32c0cae1c58afe72be3b50
Giovanny <61195397+Giovanny-DS@users.noreply.github.com>
2023-11-27 15:28:02 -0500

    Update github-app-setup.js

--- src/github-app-setup.js
+++ src/github-app-setup.js
@@ -51,8 +51,11 @@ async function obtainAppToken (id, privateKeyInput) {
       return;
     }
 
+    console.log('jwt: ', jwt)
+
     // Obtain installation id
     const octokit = getOctokit(jwt);
+    console.log('octokit: ', octokit)
     const installations = await octokit.apps.listInstallations();
     if(installations.data.length !== 1) {
       error(`App > Only 1 installation is allowed for this app. We detected it has ${installations.data.length} installations`);
